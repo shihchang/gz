7ac9e39cb6acb7bd8177b4d000a81d9c8b068112
Guoyong Zhang <36008241+guoyongzhang@users.noreply.github.com>
2018-05-08 17:14:18 -0400

    Update main.go

--- main.go
+++ main.go
@@ -14,8 +14,6 @@ type CitiesResponse struct {
 func CityHandler(res http.ResponseWriter, req *http.Request) {
     citiesResponse := &CitiesResponse{
         Cities: []string{
-			"Cairo",
-			"San Francisco",
             "Amsterdam",
             "Berlin",
             "New York",
@@ -39,7 +37,7 @@ func CityHandler(res http.ResponseWriter, req *http.Request) {
 func logHandler(w http.ResponseWriter, res *http.Request) {
     res.Write(w)
     fmt.Fprintf(w, "RemoteAddr: %s", res.RemoteAddr)
-	log.Println("Request Body: %s", res.Body)
+    log.Println("Request Body: %s", res.Body)
 }
 
 
@@ -47,7 +45,7 @@ func main() {
     log.Println("Listening on this host: http://localhost:5005")
 
     http.HandleFunc("/", logHandler)
-    http.HandleFunc("/cities.json", CityHandler)
+    http.HandleFunc("/cities", CityHandler)
     err := http.ListenAndServe(":5005", nil)
     if err != nil {
         log.Fatal("Unable to listen on :5005: ", err)
