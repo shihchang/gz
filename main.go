e9287b6a81d124c7cd8abe7bd4ce45db045af251
shihchang <36014346+shihchang@users.noreply.github.com>
2018-06-01 14:38:20 -0400

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type CitiesResponse struct {
 func CityHandler(res http.ResponseWriter, req *http.Request) {
     citiesResponse := &CitiesResponse{
         Cities: []string{
-            "NYC",
+            "NYC", "Foo",
             "LA",
             "Chicago",
             "Philly",
